80ef4ea8ee50e8d1f9ce44f3ec2ed75420d79dd5
Byron Johnson <byron@byronjohnson.net>
2022-08-10 21:45:56 -0600

    Track build artifacts in installed packages.
    Add a step to the validation phase of the modular resolver that rules out
    pre-installed package options that would introduce a missing artifact
    condition, so that alternatives if available can be chosen instead, rather than
    producing assignments for a build plan that would fail with ‘files missing’
    (e.g. ‘….hi’ or ‘….dyn_hi’).

--- cabal-install/src/Distribution/Client/ProjectConfig/Legacy.hs
+++ cabal-install/src/Distribution/Client/ProjectConfig/Legacy.hs
@@ -535,7 +535,8 @@ convertLegacyAllPackageFlags globalFlags configFlags configExFlags installFlags 
     --installShadowPkgs         = projectConfigShadowPkgs,
       installStrongFlags        = projectConfigStrongFlags,
       installAllowBootLibInstalls = projectConfigAllowBootLibInstalls,
-      installOnlyConstrained    = projectConfigOnlyConstrained
+      installOnlyConstrained    = projectConfigOnlyConstrained,
+      installRequireArtifacts   = projectConfigRequireArtifacts
     } = installFlags
 
     ProjectFlags
@@ -783,6 +784,7 @@ convertToLegacySharedConfig
       installStrongFlags       = projectConfigStrongFlags,
       installAllowBootLibInstalls = projectConfigAllowBootLibInstalls,
       installOnlyConstrained   = projectConfigOnlyConstrained,
+      installRequireArtifacts  = projectConfigRequireArtifacts,
       installOnly              = mempty,
       installOnlyDeps          = projectConfigOnlyDeps,
       installIndexState        = projectConfigIndexState,
@@ -1193,7 +1195,7 @@ legacySharedConfigFieldDescrs constraintSrc = concat
       , "max-backjumps", "reorder-goals", "count-conflicts"
       , "fine-grained-conflicts" , "minimize-conflict-set", "independent-goals", "prefer-oldest"
       , "strong-flags" , "allow-boot-library-installs"
-      , "reject-unconstrained-dependencies", "index-state"
+      , "reject-unconstrained-dependencies", "require-artifacts", "index-state"
       ]
   . commandOptionsToFields
   $ installOptions ParseArgs
